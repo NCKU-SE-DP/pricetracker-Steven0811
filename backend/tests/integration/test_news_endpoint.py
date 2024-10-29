7907471ad764e4042ddb921dd75a24c80195e229
Steven <steven950811@gmail.com>
2024-10-29 13:27:34 +0800

    fix: fix the wrong path of main.py

--- backend/tests/integration/test_news_endpoint.py
+++ backend/tests/integration/test_news_endpoint.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
 import json
 from jose import jwt
-from main import app
-from main import Base, NewsArticle, User, session_opener, user_news_association_table
-from main import NewsSumaryRequestSchema, PromptRequest
-from main import password_context
+from src.main import app
+from src.main import Base, NewsArticle, User, session_opener, user_news_association_table
+from src.main import NewsSumaryRequestSchema, PromptRequest
+from src.main import pwd_context
 from unittest.mock import Mock
 
 
@@ -38,7 +38,7 @@ def clear_users():
 
 @pytest.fixture(scope="module")
 def test_user(clear_users):
-    hashed_password = password_context.hash("testpassword")
+    hashed_password = pwd_context.hash("testpassword")
 
     with next(override_session_opener()) as db:
         user = User(username="testuser", hashed_password=hashed_password)
--- backend/tests/integration/test_user_endpoint.py
+++ backend/tests/integration/test_user_endpoint.py
@@ -2,10 +2,10 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
-from main import app
-from main import Base, User, session_opener
+from src.main import app
+from src.main import Base, User, session_opener
 from jose import jwt
-from main import password_context
+from src.main import pwd_context
 
 SECRET_KEY = "1892dhianiandowqd0n"
 ALGORITHM = "HS256"
@@ -38,7 +38,7 @@ def clear_users():
 
 @pytest.fixture(scope="module")
 def test_user(clear_users):
-    hashed_password = password_context.hash("testpassword")
+    hashed_password = pwd_context.hash("testpassword")
 
     with next(override_session_opener()) as db:
         user = User(username="testuser", hashed_password=hashed_password)
