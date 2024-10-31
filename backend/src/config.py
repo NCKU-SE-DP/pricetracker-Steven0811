b19e4a268e379078fa50ae9cf0e7a4137aebab14
Steven <steven950811@gmail.com>
2024-11-01 00:34:40 +0800

    move ALLOWED_ORIGIN to config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,3 +2,5 @@ class Sentry():
     DSN = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
     TRACE_SAMPLE_RATE = 1.0
     PROFILES_SAMPLE_RATE = 1.0
+
+ALLOWED_ORIGIN = "http://localhost:8080"
