1f3e509261cfb617cea7eadb3b73cf44191b86c0
Steven <steven950811@gmail.com>
2024-10-31 17:21:29 +0800

    refctor: move code related to sentry to config.py

---
+++ backend/src/config.py
@@ -0,0 +1,4 @@
+class Sentry():
+    dsn = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
+    trece_sample_rate = 1.0
+    profiles_sample_rate = 1.0
