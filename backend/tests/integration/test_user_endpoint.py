1d368edb0498355c852e36677226f0cdda32ba7e
Steven <steven950811@gmail.com>
2024-11-01 00:15:00 +0800

    fix: fix the path of database and models

--- backend/tests/integration/test_user_endpoint.py
+++ backend/tests/integration/test_user_endpoint.py
@@ -3,7 +3,9 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
 from src.main import app
-from src.main import Base, User, session_opener
+from src.main import session_opener
+from src.database import Base
+from src.models import User
 from jose import jwt
 from src.main import pwd_context
 
