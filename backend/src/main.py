fe8710ca81e4edb205dd586db062dd6480b8634e
Steven <steven950811@gmail.com>
2024-10-31 17:04:43 +0800

    refactor: delete models defination codes and import models from models.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -14,58 +14,11 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-from sqlalchemy import (Column, ForeignKey, Integer, String, Table, Text,
-                        create_engine)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
-
 from src.auth.schemas import UserAuthSchema
 from src.news.schemas import NewsSumaryRequestSchema, PromptRequest
 
-from src.database import Base, database_engine, SessionLocal, DatabaseSession
-
-USER_NEWS_ASSOCIATION_TABLE_NAME = "user_news_upvotes"
-USERS_TABLE_NAME = "users"
-NEWS_ARTICLES_TABLE_NAME = "news_articles"
-
-user_news_association_table = Table(
-    USER_NEWS_ASSOCIATION_TABLE_NAME,
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey(f"{USERS_TABLE_NAME}.id"), primary_key=True),
-    Column(
-        "news_articles_id", Integer, ForeignKey(f"{NEWS_ARTICLES_TABLE_NAME}.id"), primary_key=True
-    ),
-)
-
-
-MAX_USERNAME_LENGTH = 50
-MAX_PASSWORD_HASH_LENGTH = 200
-
-class User(Base):
-    __tablename__ = USERS_TABLE_NAME
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(MAX_USERNAME_LENGTH), unique=True, nullable=False)
-    hashed_password = Column(String(MAX_PASSWORD_HASH_LENGTH), nullable=False)
-    upvoted_news = relationship(
-        "NewsArticle",
-        secondary=user_news_association_table,
-        back_populates="upvoted_by_users",
-    )
-
-
-class NewsArticle(Base):
-    __tablename__ = NEWS_ARTICLES_TABLE_NAME
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    url = Column(String, unique=True, nullable=False)
-    title = Column(String, nullable=False)
-    time = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    summary = Column(Text, nullable=False)
-    reason = Column(Text, nullable=False)
-    upvoted_by_users = relationship(
-        "User", secondary=user_news_association_table, back_populates="upvoted_news"
-    )
-
+from src.database import database_engine, SessionLocal, DatabaseSession
+from src.models import user_news_association_table, User, NewsArticle
 
 SENTRY_DSN = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
 SENTRY_TRACES_SAMPLE_RATE = 1.0
@@ -130,8 +83,6 @@ from urllib.parse import quote
 import requests
 from bs4 import BeautifulSoup
 from sqlalchemy.orm import Session
-
-
 def add_news_to_db(news_data):
     """
     add new to db
