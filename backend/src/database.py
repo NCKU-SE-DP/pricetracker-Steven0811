0651393364b1b5ba246a73614195e95d71e51672
Steven <steven950811@gmail.com>
2024-11-02 12:35:23 +0800

    refactor: add session_opener to database.py

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 Base = declarative_base()
 
@@ -10,3 +11,10 @@ Base.metadata.create_all(database_engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=database_engine)
 DatabaseSession = sessionmaker(bind=database_engine)
+
+def session_opener():
+    session = Session(bind=database_engine)
+    try:
+        yield session
+    finally:
+        session.close()
