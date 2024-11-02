272fbb054c3f3cc3d3f95602ec87373557fb09b9
Steven <steven950811@gmail.com>
2024-11-02 14:10:03 +0800

    refactor: move variables related to AI to config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -8,5 +8,11 @@ class Auth():
     JWT_ALGORITHM = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
     DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES = 15
-    
+    TOKER_URL = "/api/v1/users/login"
+
+class OpenAI():
+    FIRST_CHOICE_INDEX = 0
+    AI_MODEL = "gpt-3.5-turbo"
+    ID_COUNTER_START = 1000000
+
 ALLOWED_ORIGIN = "http://localhost:8080"
