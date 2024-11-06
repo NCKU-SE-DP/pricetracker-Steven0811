7a1bbf1100f6443fef8d7ec33e04109f052d5869
Steven <steven950811@gmail.com>
2024-11-06 19:47:38 +0800

    refactor: change the class name OpenAI to AI in order to avoid the same name with OpenAI module

--- backend/src/config.py
+++ backend/src/config.py
@@ -10,7 +10,7 @@ class Auth():
     DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES = 15
     TOKER_URL = "/api/v1/users/login"
 
-class OpenAI():
+class AI():
     FIRST_CHOICE_INDEX = 0
     AI_MODEL = "gpt-3.5-turbo"
     ID_COUNTER_START = 1000000
