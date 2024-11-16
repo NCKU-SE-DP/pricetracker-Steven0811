e59d834976e66b5f5a2e79f9805d9446536add2c
Steven <steven950811@gmail.com>
2024-11-16 14:57:06 +0800

    refactor: move news utils function to NewsUtils

--- backend/src/news/utils.py
+++ backend/src/news/utils.py
@@ -3,11 +3,14 @@ from openai import OpenAI
 from src.news.constants import ID_COUNTER_START
 from src.config import AI
 
-_id_counter = itertools.count(start=ID_COUNTER_START)
+class NewsUtils(AI):
+    def __init__(self):
+        super().__init__()
+        self._id_counter = itertools.count(start=ID_COUNTER_START)
 
-def generate_ai(message):
-    ai = OpenAI(api_key="xxx").chat.completions.create(
-        model = AI.AI_MODEL,
-        messages = message,
-    )
-    return ai
+    def generate_ai(self, message):
+        ai = OpenAI(api_key="xxx").chat.completions.create(
+            model = self.AI_MODEL,
+            messages = message,
+        )
+        return ai
