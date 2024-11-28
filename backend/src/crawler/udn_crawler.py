3c47122491aa51d15bfb927c5ca4bbe027d4de9d
楊傳世 <steven950811@gmail.com>
2024-11-28 18:32:49 +0800

    [第九組] Refactor crawler implementation (#6)
    * refactor: move main.py to backend/src/
    
    * fix: fix the wrong path of main.py
    
    * refactor: move authentic-related pydentic models to schemas.py
    
    * refactor: move news-related pydentic models to schemas.py
    
    * fix: fix the path of pydentic models from schemas files
    
    * refactor: move code that related to database and session to database.py
    
    * refactor: delete code moved to database.py and import variables in database.py
    
    * refactor: move user and news models to models.py
    
    * refactor: move constants related to models to constants.py
    
    * refactor: delete models defination codes and import models from models.py
    
    * fix: fix the path of imported models
    
    * refctor: move code related to sentry to config.py
    
    * refactor: delete sentry code and import it from config.py
    
    * refactor: change variables' names int config.py to UPPER_SNAKE_CASE
    
    * fix: fix the path of database and models
    
    * move ALLOWED_ORIGIN to config.py
    
    * refactor: delete ALLOWED_ORIGIN and import it from config.py
    
    * refactor: move variables related to Auth to config.py
    
    * refactor: move routers related to users to router.py
    
    * refactor: delete user-related routers code and import it from users/router.py
    
    * refactor: add session_opener to database.py
    
    * refactor: move router dependencies to dependencies.py
    
    * refactor: move variables related to AI to config.py
    
    * refactor: move non-business logic function about news to utils.py
    
    * refactor: move routers related to news to news/router.py
    
    * refactor: delete news router code and import it from news/router.py
    
    * fix: fix the wrong  routers' prefix and path
    
    * refactor: move routers related to prices to prices/router.py
    
    * refactor: delete prices router code and import it from prices/router.py
    
    * refactor: add Basic class and put api prefix into it
    
    * refactor: import api prefix from config.py and apply it on API
    
    * refactor: move business logic related to Auth to Auth/service.py
    
    * refactor: move business logic related to News to News/service.py
    
    * refactor: move constants related to News to News/service.py
    
    * refactor: delete all business logic functions which are moved to service.py
    
    * fix: fix wrong pathes of variables and functions
    
    * refactor: move SCHEDULER_INTERVAL_MINUTES to config.py
    
    * refactor: delete SCHEDULER_INTERVAL_MINUTES and import it from config.py
    
    * refactor: change the class name OpenAI to AI in order to avoid the same name with OpenAI module
    
    * docs: add docstring to every function and delete
    
    unnecessary comments
    
    * fix: fix the path of requests.get from origin main to src.prices.router
    
    * fix: fix the wrong path of read_user_me function
    
    * fix: fix origin argument user_data of create_access_token function to data for the sake of match the name of testcase
    
    * fix: fix the wrong path of read_user_me
    
    * fix: fix the path of OpenAI
    
    * fix: fix origin article_id to id that will not conflict testcase
    
    * fix: fix the path of OpenAI
    
    * fix: fix the path of get_new_info and get in test_search_news
    
    * chore: update CI configuration
    
    * refactor: move crawler-related functions to crawler
    
    * feat: add crwler unittest
    
    * fix: change the wrong path ofimported class
    
    * fix: fix unittest failures
    
    * refactor: import udn_crawler and change the origin crawler related logic to udn_crawler methods
    
    * chore: add new branch in ci testing
    
    * fix: fix the wrong spelling of branch's name
    
    * fix: fix the ci testing error

---
+++ backend/src/crawler/udn_crawler.py
@@ -0,0 +1,145 @@
+"""
+UDN News Scraper Module
+
+This module provides the UDNCrawler class for fetching, parsing, and saving news articles from the UDN website.
+The class extends the NewsCrawlerBase and includes functionalities to search for news articles based on a search term,
+parse the details of individual articles, and save them to a database using SQLAlchemy ORM.
+
+Classes:
+    UDNCrawler: A class to scrape news from UDN.
+
+Exceptions:
+    DomainMismatchException: Raised when the URL domain does not match the expected domain for the crawler.
+
+Usage Example:
+    crawler = UDNCrawler(timeout=10)
+    headlines = crawler.startup("technology")
+    for headline in headlines:
+        news = crawler.parse(headline.url)
+        crawler.save(news, db_session)
+
+UDNCrawler Methods:
+    __init__(self, timeout: int = 5): Initializes the crawler with a default timeout for HTTP requests.
+    startup(self, search_term: str) -> list[Headline]: Fetches news headlines for a given search term across multiple pages.
+    get_headline(self, search_term: str, page: int | tuple[int, int]) -> list[Headline]: Fetches news headlines for specified pages.
+    _fetch_news(self, page: int, search_term: str) -> list[Headline]: Helper method to fetch news headlines for a specific page.
+    _create_search_params(self, page: int, search_term: str): Creates the parameters for the search request.
+    _perform_request(self, params: dict): Performs the HTTP request to fetch news data.
+    _parse_headlines(response): Parses the response to extract headlines.
+    parse(self, url: str) -> News: Parses a news article from a given URL.
+    _extract_news(soup, url: str) -> News: Extracts news details from the BeautifulSoup object.
+    save(self, news: News, db: Session): Saves a news article to the database.
+    _commit_changes(db: Session): Commits the changes to the database with error handling.
+"""
+
+import requests
+from bs4 import BeautifulSoup
+from sqlalchemy.orm import Session
+from .crawler_base import NewsCrawlerBase, Headline, News, NewsWithSummary
+
+class UDNCrawler(NewsCrawlerBase):
+    CHANNEL_ID = 2
+
+    def __init__(self, timeout: int = 5) -> None:
+        self.news_website_url = "https://udn.com/api/more"
+        self.timeout = timeout
+
+    def startup(self, search_term: str) -> list[Headline]:
+        """
+        Initializes the application by fetching news headlines for a given search term across multiple pages.
+        This method is typically called at the beginning of the program when there is no data available,
+        hence it fetches headlines from the first 10 pages.
+
+        :param search_term: The term to search for in news headlines.
+        :return: A list of Headline namedtuples containing the title and URL of news articles.
+        :rtype: list[Headline]
+        """
+        return self.get_headline(search_term, page=(1, 10))
+
+    def get_headline(
+        self, search_term: str, page: int | tuple[int, int]
+    ) -> list[Headline]:
+
+        # Calculate the range of pages to fetch news from.
+        # If 'page' is a tuple, unpack it and create a range representing those pages (inclusive).
+        # If 'page' is an int, create a list containing only that single page number.
+        # page_range = range(*page) if isinstance(page, tuple) else [page]
+        if isinstance(page, tuple):
+            start_page, end_page = page
+            page_range = range(start_page, end_page + 1)
+        else:
+            page_range = [page]
+
+        headlines = []
+        for page_num in page_range:
+            headlines.extend(self._fetch_news(page_num, search_term))
+
+        return headlines
+
+    def _fetch_news(self, page: int, search_term: str) -> list[Headline]:
+        params = self._create_search_params(page, search_term)
+        response = self._perform_request(params=params)
+        headlines = self._parse_headlines(response)
+        return headlines
+
+    def _create_search_params(self, page: int, search_term: str) -> dict:
+        return {
+            "page": page,
+            "id": f"search:{search_term}",
+            "channelId": self.CHANNEL_ID,
+            "type": "searchword",
+        }
+
+    def _perform_request(self, url: str | None = None, params: dict | None = None) -> requests.Response:
+        url = url or self.news_website_url
+        response = requests.get(url, params=params, timeout=self.timeout)
+        response.raise_for_status()
+        return response
+
+    @staticmethod
+    def _parse_headlines(response: requests.Response) -> list[Headline]:
+        data = response.json()
+        headlines = []
+        for news_item in data["lists"]:
+            headline = Headline(
+                title=news_item["title"],
+                url=news_item["titleLink"],
+            )
+            headlines.append(headline)
+        return headlines
+
+    def parse(self, url: str) -> News:
+        response = self._perform_request(url=url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        news = self._extract_news(soup, url)
+        return news
+
+    @staticmethod
+    def _extract_news(soup: BeautifulSoup, url: str) -> News:
+        article_title = soup.find("h1", class_="article-content__title").text
+        time = soup.find("time", class_="article-content__time").text
+        content_section = soup.find("section", class_="article-content__editor")
+        content = " ".join(
+            paragraph.text
+            for paragraph in content_section.find_all("p")
+            if paragraph.text.strip() != "" and "▪" not in paragraph.text
+        )
+
+        return News(
+            url=url,
+            title=article_title,
+            time=time,
+            content=content,
+        )
+
+    def save(self, news: NewsWithSummary, db: Session):
+        db.add(news)
+        self._commit_changes(db)
+
+    @staticmethod
+    def _commit_changes(db: Session):
+        try:
+            db.commit()
+        except Exception as e:
+            db.rollback()
+            raise e
